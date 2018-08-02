d58a59cc8dc47dd804fd1d8479b35ab0a142df55
tekleayele@gmail.com <tekleayele@gmail.com>
2018-08-02 16:17:38 -0400

    Initial Commit.

---
+++ src/main/java/org/tekle/spring/boot/ldapAuth/HomeController.java
@@ -0,0 +1,14 @@
+package org.tekle.spring.boot.ldapAuth;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Welcome to the home page!";
+    }
+
+}
---
+++ src/main/java/org/tekle/spring/boot/ldapAuth/LdapSpringAuthApplication.java
@@ -0,0 +1,13 @@
+package org.tekle.spring.boot.ldapAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapSpringAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdapSpringAuthApplication.class, args);
+    }
+
+} 
